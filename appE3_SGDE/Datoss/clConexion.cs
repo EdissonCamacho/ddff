974ddecc031ee7416ec2582e109f949bf0245b39
Diego Figue <torogim@hotmail.com>
2021-03-04 09:37:51 -0500

    Creacion carpeta de vista y datos, clase conexion pendiente

---
+++ appE3_SGDE/Datoss/clConexion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+using System.Data;
+
+namespace appE3_SGDE.Datoss
+{
+    class clConexion
+    {
+        MySqlConnection objConexion = null;
+
+        public clConexion()
+        {
+            try
+            {
+                objConexion = new MySqlConnection("server=localhost; user id=root;Port=3306;database=domiciliosbd;Password=2142457");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Message);
+                throw;
+            }
+
+
+
+
+
+        }
+        public DataTable mtdDesconectado(string consulta)
+        {
+            objConexion.Open();
+            MySqlDataAdapter adaptador = new MySqlDataAdapter(consulta, objConexion);
+            DataTable tblDatos = new DataTable();
+            adaptador.Fill(tblDatos);
+            objConexion.Close();
+            return tblDatos;
+
+
+
+        }
+    }
+}

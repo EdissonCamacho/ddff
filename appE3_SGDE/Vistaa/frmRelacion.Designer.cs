c63ed2ec4b8c922d7ebf5c5b3bbf17eed2952228
JhonathanAlarc <jhonfercho43@gmail.com>
2021-03-09 11:17:46 -0500

    Creacion de Formulario y Clase "Relacion"

---
+++ appE3_SGDE/Vistaa/frmRelacion.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace appE3_SGDE.Vistaa
+{
+    partial class frmRelacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmRelacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::appE3_SGDE.Properties.Resources.WhatsApp_Image_2021_03_09_at_8_22_41_AM;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmRelacion";
+            this.Text = "frmRelacion";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}

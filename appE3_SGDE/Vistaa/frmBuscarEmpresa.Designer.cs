00d84b9e1811f4843f227da0fd14040fcb1bfe19
Amanda-Acero <laacero4@misena.edu.co>
2021-03-14 19:19:49 -0500

    Creación del formulario frmBuscarEmpresa

---
+++ appE3_SGDE/Vistaa/frmBuscarEmpresa.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace appE3_SGDE.Vistaa
+{
+    partial class frmBuscarEmpresa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Empresas_Registradas = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Demi", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(219, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sector";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(273, 74);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(213, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(492, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 49);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Buscar Empresa";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Empresas_Registradas
+            // 
+            this.Empresas_Registradas.FormattingEnabled = true;
+            this.Empresas_Registradas.Location = new System.Drawing.Point(223, 118);
+            this.Empresas_Registradas.Name = "Empresas_Registradas";
+            this.Empresas_Registradas.Size = new System.Drawing.Size(344, 212);
+            this.Empresas_Registradas.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(697, 39);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "EMPRESAS REGISTRADAS POR SECTORES";
+            // 
+            // frmBuscarEmpresa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Empresas_Registradas);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmBuscarEmpresa";
+            this.Text = "frmBuscarEmpresa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox Empresas_Registradas;
+        private System.Windows.Forms.Label label2;
+    }
+}
